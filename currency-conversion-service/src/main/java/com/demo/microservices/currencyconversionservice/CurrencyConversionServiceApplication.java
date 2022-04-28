bcd1eec9c318ce1ed09132d643cb90c34c186ac9
JuanEstebanKatz <jestebankatz@gmail.com>
2022-04-27 22:33:35 -0500

    adding for ribbon

--- currency-conversion-service/src/main/java/com/demo/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/demo/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -6,8 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 //import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-
-
 @SpringBootApplication
 @EnableFeignClients("com.demo.microservices.currencyconversionservice")
 @EnableDiscoveryClient
