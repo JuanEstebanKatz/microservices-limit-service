e48f011672cc4cac46cff5d288a91955ca5db315
JuanEstebanKatz <jestebankatz@gmail.com>
2022-04-27 19:53:52 -0500

    Hasta este punto funciona sin utilizar Ribbon, hasta Feign

---
+++ currency-conversion-service/src/main/java/com/demo/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,16 @@
+package com.demo.microservices.currencyconversionservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+// se quita la URL estatica ya que se van a tener múltiples instancias del servicio corriendo.
+@FeignClient(name = "currency-conversion-service", url = "localhost:8000")
+//@FeignClient(name = "currency-conversion-service")
+//@RibbonClient(name = "currency-conversion-service")
+public interface CurrencyExchangeServiceProxy {
+
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
+
+}
